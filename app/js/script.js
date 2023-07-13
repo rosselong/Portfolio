a3194791b8a055b01b5ee57a59d691f41a18cd7a
rossel ong <rosselong10@gmail.com>
2023-07-13 19:41:15 +0800

    commit

--- app/js/script.js
+++ app/js/script.js
@@ -3,6 +3,7 @@ const menuEl = document.querySelector("#menu__bar");
 const toggleEl = document.querySelector("#btntoggle");
 const linksEl = document.querySelector(".links");
 const noscrollEl = document.querySelector("#body");
+const navlinkEl = document.querySelectorAll(".nav__links");
 
 toggleEl.addEventListener("click", () => {
   if (menuEl.classList.contains("menu__open")) {
@@ -32,28 +33,16 @@ document.addEventListener("scroll", () => {
 
   if (menuEl.classList.contains("menu__open")) {
     header.classList.remove("scrolled");
-  } else {
-    header.classList.add("scrolled");
+    noscrollEl.classList.add("noscroll");
   }
 });
 
-// window.addEventListener("scroll", () => {
-//   const menuEl = document.querySelector("#menu__bar");
-//   const navlinkEl = document.querySelectorAll(".nav__links");
-//   const sectionEl = document.querySelectorAll("section");
-//   let currentSection = "home";
-
-//   sectionEl.forEach((sectionEl) => {
-//     if (window.scrollY >= sectionEl.offsetTop - 400) {
-//       currentSection = sectionEl.id;
-//     }
-//   });
-
-//   navlinkEl.forEach((navlinkEl) => {
-//     if (navlinkEl.href.includes(currentSection)) {
-//       navlinkEl.classList.add("active");
-//     } else {
-//       navlinkEl.classList.remove("active");
-//     }
-//   });
-// });
+navlinkEl.addEvenListener("click", () => {
+  navlinkEl.forEach((navlinkEl) => {
+    if (navlinkEl.href.includes(currentSection)) {
+      navlinkEl.classList.add("active");
+    } else {
+      navlinkEl.classList.remove("active");
+    }
+  });
+});
